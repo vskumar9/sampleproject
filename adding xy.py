5cd23126abf69cf1d3094606b036f7ce145fd72d
vskumar <72122950+vskumar9@users.noreply.github.com>
2022-04-13 09:54:14 +0530

    update

---
+++ adding xy.py
@@ -0,0 +1,7 @@
+#1st program inputs giving in a code
+x,y=4,3
+print((x+y)*(x+y))
+#2nd program inputs giving users
+x=int(input('Enter the "x" value : '))
+y=int(input('Enter the "y" value : '))
+print('(x+y)*(x+y) : ',(x+y)*(x+y))
