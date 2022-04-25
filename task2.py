b3e230e73b86e8ab5bd841e52e87932e0cf83999
vskumar <72122950+vskumar9@users.noreply.github.com>
2022-04-25 22:18:21 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,6 +0,0 @@
-a=int(input('Enter the 1st number'))
-b=int(input('Enter the 2nd number'))
-if a==b==5 :
-    print('Given numbers are same number : ',a)
-elif a+b==5 or a-b==5:
-    print('The given values difference or sum : 5')
